9628643b48ec6efc10f21477c82dcc5a1060f5a7
JONPIRES <jonbpires@gmail.com>
2023-03-27 17:40:55 -0300

    inicial set up for django app

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+kids = [
+    {'name':'Julio', 'age': 2, 'nickname': 'Bud'},
+    {'name':'William', 'age': 4, 'nickname': 'Willy'}
+]
+
+def home(req):
+    return render(req, 'home.html')
+
+def about(req):
+    return render(req,'about.html')
+
+def kids_index(req):
+    return render(req,'kids/index.html',{
+        'kids':kids
+    })
